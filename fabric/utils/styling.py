becdfb84a45f1d4cf7f091840b76963617ff84b8
uiriansan <uiriansan@gmail.com>
2025-04-19 11:19:05 -0300

    Move shell to its own repo

--- fabric/utils/styling.py
+++
@@ -1,6 +0,0 @@
-def int_to_ordinal(n: int):
-    if 11 <= (n % 100) <= 13:
-        suffix = "th"
-    else:
-        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
-    return str(n) + suffix
